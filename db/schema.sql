5ce0202025a1166f07701d8c81deef3bb96bae97
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-10-23 06:36:12 -0700

    fixed schema.db

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
+  department_id INTEGER,
   FOREIGN KEY (department_id)
    REFERENCES department(id)
 );
@@ -15,7 +16,9 @@ CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
   FOREIGN KEY (role_id) REFERENCES role(id),
+  manager_id INTEGER,
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
