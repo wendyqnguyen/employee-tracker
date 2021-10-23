bc19282bc10a407ecbb0dec97f65985d7412c2da
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-10-23 06:23:05 -0700

    schema.sql and seed.sql

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department (name)
+VALUES
+('Management'),
+('Operations'),
+('Marketing and Sales'),
+('Research and Development'),
+('Finance')
+
+INSERT INTO role (title, salary)
+VALUES
+  ('Administrative Assistant', 65000),
+  ('Executive Assistant', 77000), 
+  ('Marketing Manager', 170000),
+  ('Customer Service Representative', 59000),
+  ('Nurse Practitioner', 150000),
+  ('Software Engineer', 125000),
+  ('Sales Manager', 120000),
+  ('Data Entry Clerk', 45000)
+
+INSERT INTO employee (first_name, last_name, role_id, department_id)
+VALUES
+  ('Ronald', 'Firbank', '', ''),
+  ('Virginia', 'Woolf', '', ''), 
+  ('Piers', 'Gaveston', '', ''),
+  ('Charles', 'LeRoi', '', ''),
+  ('Katherine', 'Mansfield', '', ''),
+  ('Dora', 'Carrington', '', ''),
+  ('Edward', 'Bellamy', '', ''),
+  ('Montague', 'Summers', '', ''),
+  ('Octavia', 'Butler', '', ''),
+  ('Unica', 'Zurn', '', ''),
