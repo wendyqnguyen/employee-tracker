8c9010c3632543941a1c59a8257e31bece75e7de
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-10-27 07:07:34 -0700

    add department and role

--- server.js
+++ server.js
@@ -23,34 +23,9 @@ const db = mysql.createPool(
     console.log('Connected to the company database.')
   );
 
-const addDepartment = () =>{
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "Please enter the department name >> "
-        }])
-    .then ( answer => {
-        const errors = inputCheck(answer, 'name');
-        if (errors) {
-        console.log({ error: errors });
-        return;
-        }
-        const sql = `INSERT INTO department (name)
-    VALUES (?)`;
-        const params = [answer.name];
-        db.query(sql, params, (err, result) => {
-            if (err) {
-                console.log({ error: err.message });
-                return;
-            } else {
-                console.log(`${answer.name} was added to the database`);
-            }
-        });
+//   const promisePool = db.promise();
 
-        
-    })
-}
+  
 
 const addEmployee = () =>{
     inquirer.prompt([
@@ -101,95 +76,13 @@ async function getDeparments() {
     return Promise.resolve(str);
 }
 
-async function addRole () {
-    let department_id;
-    let result = await db.execute("SELECT name FROM department;");
-    let departments = result[0];
-    departments = JSON.stringify(departments);
-    departments = departments.replace(/"name":/g, '');
-    departments = departments.replace(/}/g, '');
-    departments = departments.replace(/{/g, '');
-    departments = JSON.parse(departments);
 
-     inquirer.prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: "Please enter the role title >> "
-            },
-            {
-                type: 'input',
-                name: 'salary',
-                message: "Please enter the role salary >> "
-            },
-            {
-                type: 'list',
-                name: 'department_name',
-                message: "Please enter the role department >> ",
-                choices: departments
-            }])
-            .then ( async (answers) => {
-                let params = answers.department_name;
 
-                sql = `INSERT INTO roles (title, salary, department_id)
-                VALUES ("${answers.title}", ${answers.salary},
-                       (SELECT id FROM department WHERE name ="${answers.department_name}"));`;
-                       
-                    
-                try {
-                    result = await db.query(sql);
-                    console.log(`${answers.title} was added to roles.`)
-                } catch (err) {
-                    console.log({ error: err.message });
-                    return;
-                } 
- });
-}
-async function mainPrompt () {
-
-    inquirer.prompt( [
-        {
-            type: 'list',
-            name: 'actions',      
-            message: 'Please select one of the following options:',
-            choices: ['View all departments', 
-            'View all roles', 
-            'View all employees', 
-            'Add a department', 
-            'Add a role', 
-            'Add an employee', 
-            'Update an employee role']
-        }])
-    .then(answer => {
-        //If 'View all departments' is chosen
-        //call displayDepartmentNames() to display all departments
-        if (answer.actions === 'View all departments') {
-            dbUtils.getDepartmentNames();
-                return mainPrompt();
-              
-            // dbUtils.displayDepartmentNames();
-            //     return mainPrompt();
-        } else if (answer.actions === 'View all roles'){
-            dbUtils.getRoles();
-            return mainPrompt();
-        } else if (answer.actions === 'View all employees'){
-            dbUtils.getEmployees();
-            return mainPrompt();
-        }
-    //   } else if (answer.actions === 'Add an engineer') {
-    //       //if 'Add an engineer' is selected call the function to prompt for engineer info
-    //       return promptEngineer();
-    //   } else if (answer.actions === 'Add an intern') {
-    //     //if 'Add an intern' is selected call the function to prompt for intern info
-    //         return promptIntern();
-    //     }
-    });
-  };
 
   console.log(`
-    =====================
-    Employee Tracker
-    =====================
-    `);
+=====================
+Employee Tracker
+=====================
+`);
 
-  mainPrompt();
+  dbUtils.mainPrompt();
--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,5 +1,6 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const cTable = require('console.table');
+const inquirer = require('inquirer');
 
 
 // Connect to database
@@ -18,6 +19,15 @@ const db = mysql.createPool(
   console.log('Connected to the company database.')
 );
 
+//Print the data in a table with the given title
+function printDataTable(title, data) {
+  console.log(`
+---------------------------------------------------
+${title.toUpperCase()}
+---------------------------------------------------`);
+  console.table(data);
+  console.log(`----------------------------------------------------------`);
+}
 function getDepartmentID (departmentName) {
     const params = [];
     params[0] = departmentName;
@@ -49,41 +59,165 @@ function getDepartmentID (departmentName) {
       });
     }
 
-  function getEmployees(){
-    let employeeArr = [];
-    const sql = `SELECT * FROM employee`;
-    db.query(sql, (err,rows) =>{
-        if (err) {
-            console.log({ error: err.message});
-            return;
-        }
-        for(i = 0; i < rows.length; i++){
-          employeeArr[i] = {ID: rows[i].id,
-            First_Name: rows[i].first_name,
-            Last_Name: rows[i].last_name
-          }
-        }
-        console.table(employeeArr);
-    });
+  async function getDeparments() {
+    const result = await db.query("SELECT * FROM department ORDER BY name ASC");
+    return result[0];
+  }
+  
+async function getEmployees(){
+
+  const result = await db.query("SELECT * FROM employee");
+  return result[0];
   }
 
-function getRoles(){
-    let rolesArr = [];
-    const sql = `SELECT roles.id, roles.title, roles.salary, department.name  FROM roles, department WHERE roles.department_id = department.id;`;
-    db.query(sql, (err,rows) =>{
-        if (err) {
-            console.log({ error: err.message});
-            return;
-        }
-        for(i = 0; i < rows.length; i++){
-          rolesArr[i] = {ID: rows[i].id,
-            Title: rows[i].title,
-            Salary: rows[i].salary,
-            Deparment: rows[i].name
-          }
-        }
-        console.table(rolesArr);
-    });
+async function getRoles(){
+  const result = await db.query("SELECT roles.id, roles.title, roles.salary, department.name  FROM roles, department WHERE roles.department_id = department.id;");
+  return result[0];
   }
 
-  module.exports = {getDepartmentID, getDepartmentNames, getEmployees, getRoles};
+async function addRole () {
+    console.log(`
+=====================
+Add Role
+=====================
+`);
+
+
+let department_id;
+let result = await db.execute("SELECT name FROM department;");
+let departments = result[0];
+departments = JSON.stringify(departments);
+departments = departments.replace(/"name":/g, '');
+departments = departments.replace(/}/g, '');
+departments = departments.replace(/{/g, '');
+departments = JSON.parse(departments);
+
+  inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: "Please enter the role title >> "
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: "Please enter the role salary >> "
+    },
+    {
+        type: 'list',
+        name: 'department_name',
+        message: "Please enter the role department >> ",
+        choices: departments
+    }
+  ])
+  .then ((answer) => {
+  return insertRole(answer);
+    
+  })
+
+     
+}
+
+async function insertRole(values) {
+  try {
+    const sql = `INSERT INTO roles 
+    SET title = "${values.title}",
+    salary = ${values.salary},
+    department_id = (
+        SELECT id
+          FROM department
+          WHERE name = "${values.department_name}")`;
+    const result = await db.query(sql);
+    console.log(values.title + " role was added.")
+    return mainPrompt();
+  } catch (error) {
+    return error;
+  }
+}
+
+async function addDepartment (){
+  
+console.log(`
+=====================
+Add Department
+=====================
+`);
+  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'name',
+          message: "Please enter the department name >> "
+      }])
+  .then ((answer) => {
+    return insertDepartment(answer.name);
+      
+  })
+}
+
+async function insertDepartment(name) {
+  try {
+    const sql = "INSERT INTO department (name) VALUES (?)";
+    params = [name];
+    const result = await db.query(sql, params);
+    console.log(name + " department was added.")
+    return mainPrompt();
+  } catch (error) {
+    return error;
+  }
+}
+
+async function mainPrompt () {
+
+  inquirer.prompt( [
+      {
+          type: 'list',
+          name: 'actions',      
+          message: 'Please select one of the following options:',
+          choices: ['View all departments', 
+          'View all roles', 
+          'View all employees', 
+          'Add a department', 
+          'Add a role', 
+          'Add an employee', 
+          'Update an employee role']
+      }])
+  .then(answer => {
+      //If 'View all departments' is chosen
+      //call displayDepartmentNames() to display all departments
+      if (answer.actions === 'View all departments') {
+          console.clear();
+          getDeparments().then((departments) => {
+              printDataTable("Departments", departments);
+              return mainPrompt();
+          })
+            
+      } else if (answer.actions === 'View all roles'){
+          console.clear();
+          getRoles().then((roles) => {
+              printDataTable("Roles", roles);
+              return mainPrompt();
+          })
+      } else if (answer.actions === 'View all employees'){
+          console.clear();
+          getEmployees().then((roles) => {
+              printDataTable("Employees", roles);
+              return mainPrompt();
+          })
+      } else if (answer.actions === 'Add a role'){
+        console.clear();
+        addRole();
+      } else if (answer.actions === 'Add a department'){
+          console.clear();
+          addDepartment();
+      }
+  //   } else if (answer.actions === 'Add an engineer') {
+  //       //if 'Add an engineer' is selected call the function to prompt for engineer info
+  //       return promptEngineer();
+  //   } else if (answer.actions === 'Add an intern') {
+  //     //if 'Add an intern' is selected call the function to prompt for intern info
+  //         return promptIntern();
+  //     }
+  });
+};
+
+    module.exports = {getDepartmentID, getDeparments, getEmployees, getRoles, addRole, getDepartmentNames, addDepartment, mainPrompt};
