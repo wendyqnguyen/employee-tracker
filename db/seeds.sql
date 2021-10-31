26902ad59bfaa0743e4532e0acdd9c6dbfc85750
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-10-31 08:50:31 -0700

    fixed minor bugs and updated readme

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,7 +19,7 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Ronald', 'Firbank', 1, 1),
+  ('Ronald', 'Firbank', 1, 6),
   ('Virginia', 'Woolf', 2, 1),
   ('Piers', 'Gaveston', 3, 3),
   ('Charles', 'LeRoi', 4, 2),
